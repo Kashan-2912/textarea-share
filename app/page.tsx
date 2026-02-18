9d681a43925a44b4e99d1d32a62652b47c5b6e41
Muhammad Kashan Ashraf <mkashan2912@gmail.com>
2026-02-19 01:56:20 +0500

    feat: successful implementation of compress, decompress

--- app/page.tsx
+++ app/page.tsx
@@ -18,6 +18,27 @@ function decompress(encoded : string) : string{
 export default function Home() {
   const [text, setText] = useState("");
 
+  // runs on first render, decompresses text from URL
+  useEffect(() => {
+     const hash = window.location.hash.slice(1); // remove the '#' character
+     const decoded = decompress(hash)
+
+     
+     // eslint-disable-next-line react-hooks/set-state-in-effect
+     setText(decoded);
+  }, []);
+
+  // runs everytime the text in URL changes
+  useEffect(() => {
+    if(!text){
+      window.history.replaceState(null, "", "#");
+      return;
+    }
+
+    const compressed = compress(text);
+    window.history.replaceState(null, "", `#${compressed}`);
+  }, [text]);
+
   return (
     <main style={{ padding: 40 }}>
       <h1>Live URL Compressor</h1>
